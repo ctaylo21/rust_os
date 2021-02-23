74670d525c2e30f71e0ddfb0eb2eb8ff1a6a2cdf
Caleb Taylor <caleb89taylor@gmail.com>
2021-02-23 12:09:16 -0800

    test(os): added unit and integration tests with support to writing to host machine stdio

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
+// in src/main.rs
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use rust_os::println;
 
-mod vga_buffer;
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
 
-/// This function is called on panic
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    loop {}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
 }
